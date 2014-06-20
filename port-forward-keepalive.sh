40eccf571539919a1bfbe90a8d883da7e8e03b9b
Neal Hardesty <nealhardesty@yahoo.com>
2014-06-20 15:52:06 -0700

    now allows alt local hosts

--- port-forward-keepalive.sh
+++ port-forward-keepalive.sh
@@ -8,15 +8,17 @@
 #  ClientAliveCountMax 3
 # to ensure sshd cleans up after a disconnect.
 
-HOST="$1"
+REMOTEHOST="$1"
 REMOTEPORT="$2"
-LOCALPORT="$3"
+LOCALHOST="$3"
+LOCALPORT="$4"
 
 if [ -z "$HOST" -o -z "$REMOTEPORT" ]; then
-	echo Usage: $0 '<remote hostname> <remote port> [local port (22)]'
+	echo Usage: $0 '<remote hostname> <remote port> [local host (localhost)] [local port (22)]'
 	exit 255
 fi
 
+if [ -z "$LOCALHOST" ]; then LOCALHOST=127.0.0.1; fi
 if [ -z "$LOCALPORT" ]; then LOCALPORT=22; fi
 
 trap "{ echo kthxbai; exit 0; }" SIGINT
@@ -26,9 +28,9 @@ while true; do
 	ping -o 8.8.8.8 >> /dev/null 2>&1
 	echo "done. "
 
-	echo $(date "+%H:%M:%S") Attempting to connect $HOST:$REMOTEPORT to localhost:$LOCALPORT
+	echo $(date "+%H:%M:%S") Attempting to connect $HOST:$REMOTEPORT to $LOCALHOST:$LOCALPORT
 
-	ssh -N -o "ExitOnForwardFailure yes" -o "ServerAliveCountMax 3" -o "ServerAliveInterval 5" -R '*':$REMOTEPORT:127.0.0.1:$LOCALPORT $HOST
+	ssh -N -o "ExitOnForwardFailure yes" -o "ServerAliveCountMax 3" -o "ServerAliveInterval 5" -R '*':$REMOTEPORT:$LOCALHOST:$LOCALPORT $REMOTEHOST
 	echo $(date "+%H:%M:%S") ssh exited with $?.  Will attempt reconnect.
 
 	sleep 1
